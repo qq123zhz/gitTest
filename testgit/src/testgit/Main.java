569334b84a3606d53ccf63052708866609ed61ba
qq123zhz <qq123zhz@gmail.com>
2015-03-30 16:52:37 +0800

    ttttt

--- testgit/src/testgit/Main.java
+++ testgit/src/testgit/Main.java
@@ -27,7 +27,8 @@ public class Main
 	public static void main(String[] args)
 	{
 		System.out.println("test");
-		System.out.println("test");
+		System.out.println("test");  
+		System.out.println("test");  
 	}
 }
 
