14a0282472043edec8123f82799a46fd7dc58f47
mikechen-mc <fisher.zheng@missionsky.com>
2015-03-30 17:45:23 +0800

    c1

--- testgit/src/testgit/Main.java
+++ testgit/src/testgit/Main.java
@@ -26,7 +26,7 @@ public class Main
 	
 	public static void main(String[] args)
 	{
-		System.out.println("test");
+		System.out.println("test123");
 		System.out.println("test");  
 		System.out.println("test");     
 		System.out.println("test");     
