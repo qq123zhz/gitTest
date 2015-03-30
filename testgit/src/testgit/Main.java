2892696ce35aa2b5853a2a2e95b8cb076ac625e9
qq123zhz <qq123zhz@gmail.com>
2015-03-30 16:05:20 +0800

    test

---
+++ testgit/src/testgit/Main.java
@@ -0,0 +1,35 @@
+package testgit;
+/**
+ * <pre>
+ * 
+ *  Accela Automation
+ *  File: Main.java
+ * 
+ *  Accela, Inc.
+ *  Copyright (C): 2015
+ * 
+ *  Description:
+ *  TODO
+ * 
+ *  Notes:
+ * 	$Id: Main.java 72642 2009-01-01 20:01:57Z ACHIEVO\fisher.zheng $ 
+ * 
+ *  Revision History
+ *  &lt;Date&gt;,			&lt;Who&gt;,			&lt;What&gt;
+ *  Mar 30, 2015		fisher.zheng		Initial.
+ *  
+ * </pre>
+ */
+public class Main
+{
+
+	
+	public static void main(String[] args)
+	{
+		System.out.println("test");
+	}
+}
+
+/*
+*$Log: av-env.bat,v $
+*/
